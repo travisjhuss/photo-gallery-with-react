1e969b485361a4aa99df533d1b581c32796f7002
Travis Huss <travisjhuss@gmail.com>
2021-01-15 17:33:57 -0600

    added objects of photos to gallery.data

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,18 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/withStick.jpg', description: 'This is my dog, Morton. We call him Morty for short.', likes: 0 },
+    { id: 2, path: 'images/mortyPuppy.jpg', description: 'This is when he was 4 months old and we met him at the humane society.', likes: 0 },
+    { id: 3, path: 'images/fleas.jpg', description: 'He was being treated for fleas so we had to wait to take him home.', likes: 0 },
+    { id: 4, path: 'images/puppySleep.jpg', description: 'He was able to get cozy at home quickly.', likes: 0 },
+    { id: 5, path: 'images/inBedWithJasper.jpg', description: 'He quickly became buds with our other dog, Jasper.', likes: 0 },
+    { id: 6, path: 'images/mortyStanding.jpg', description: 'He grew quickly and was soon able to get into all kinds of trouble.', likes: 0 },
+    { id: 7, path: 'images/withJasperInCar.jpg', description: 'In no time he was the same size as his brother.', likes: 0 },
+    { id: 8, path: 'images/atDayCare.jpg', description: 'He loves playing in the kiddie pool at daycare.', likes: 0 },
+    { id: 9, path: 'images/twisting.jpg', description: 'He finds new ways to lay every day.', likes: 0 },
+    { id: 10, path: 'images/lookingOutWindow.jpg', description: 'He often ponders the big questions in life.', likes: 0 },
+    { id: 11, path: 'images/almostDead.jpg', description: `A year ago, we almost lost Morty when he ate something he shouldn't have.`, likes: 0 },
+    { id: 12, path: 'images/inTshirt.jpg', description: `He recovered from surgery though and had to wear a t-shirt to protect his stitches.`, likes: 0 },
+    { id: 13, path: 'images/distinguished.jpg', description: 'He hoards toys from his brother.', likes: 0 },
+    { id: 14, path: 'images/inSunlight.jpg', description: 'Like Sheryl Crow, he wants to soak up the sun.', likes: 0 }
 ];
 
 module.exports = galleryItems;

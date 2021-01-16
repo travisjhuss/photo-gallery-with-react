c4eb1f1f7334db2c3abeb4e3664f976f4d4e8272
Travis Huss <travisjhuss@gmail.com>
2021-01-16 08:54:56 -0600

    like button PUT route updated for database. working

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -5,17 +5,17 @@ const pool = require('../modules/pool');
 
 // DO NOT MODIFY THIS FILE FOR BASE MODE
 
-// PUT Route
-router.put('/like/:id', (req, res) => {
-    console.log(req.params);
-    const galleryId = req.params.id;
-    for(const galleryItem of galleryItems) {
-        if(galleryItem.id == galleryId) {
-            galleryItem.likes += 1;
-        }
-    }
-    res.sendStatus(200);
-}); // END PUT Route
+// // PUT Route
+// router.put('/like/:id', (req, res) => {
+//     console.log(req.params);
+//     const galleryId = req.params.id;
+//     for(const galleryItem of galleryItems) {
+//         if(galleryItem.id == galleryId) {
+//             galleryItem.likes += 1;
+//         }
+//     }
+//     res.sendStatus(200);
+// }); // END PUT Route
 
 // // GET route for gallery.data
 // router.get('/', (req, res) => {
@@ -24,7 +24,7 @@ router.put('/like/:id', (req, res) => {
 
 // GET route for database
 router.get('/', (req, res) => {
-    const sqlText = `SELECT * FROM "gallery";`;
+    const sqlText = `SELECT * FROM "gallery" ORDER BY "id" ASC;`;
     pool.query(sqlText)
         .then((result) => {
             console.log('from db', result.data);
@@ -36,5 +36,26 @@ router.get('/', (req, res) => {
         })
 
 })
+
+// PUT route for database
+router.put('/like/:id', (req, res) => {
+    console.log(req.params);
+    const galleryId = req.params.id;
+    console.log('Delete route with id of', galleryId);
+
+    const query = `
+        UPDATE "gallery" 
+        SET "likes" = "likes" + 1
+        WHERE "id" = $1;
+        `;
+
+    pool.query(query, [galleryId])
+        .then((result) => {
+            res.sendStatus(204);
+        }).catch((error) => {
+            console.log('error');
+            res.sendStatus(500);
+        })
+});
 
 module.exports = router;

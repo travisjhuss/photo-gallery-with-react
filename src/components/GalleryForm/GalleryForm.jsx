bf993d18d0e752162b29340dcdba0ea52e62930c
Travis Huss <travisjhuss@gmail.com>
2021-01-16 15:44:41 -0600

    form moved to drawer, styled

--- src/components/GalleryForm/GalleryForm.jsx
+++ src/components/GalleryForm/GalleryForm.jsx
@@ -1,3 +1,4 @@
+import { TextField, Typography, Button, Drawer, createMuiTheme } from '@material-ui/core';
 import './GalleryForm.css';
 
 function GalleryForm({
@@ -8,25 +9,45 @@ function GalleryForm({
     setPhotoPath
 }) {
     return (
-        <form className="input-form" onSubmit={addPhoto}>
-            <div>Add a photo of your best friend...</div>
-            <label htmlFor="url-input">Photo url: </label>
-            <input
-                id="url-input"
-                placeholder="ex. images/photo.jpg"
-                value={photoPath}
-                onChange={e => setPhotoPath(e.target.value)}
-            />
-            <br />
-            <label htmlFor="description-input">Tell me about your photo: </label>
-            <input
-                id="description-input"
-                value={photoDescription}
-                onChange={e => setPhotoDescription(e.target.value)}
-            />
-            <br />
-            <button type="submit" onClick={addPhoto}>Submit</button>
-        </form>
+        <Drawer
+            variant="permanent"
+            anchor="bottom"
+        >
+            <form className="input-form" onSubmit={addPhoto}>
+                <Typography
+                    variant="h6"
+                >
+                    Add a photo of your best friend...</Typography>
+                <TextField
+                    label="Photo url:"
+                    variant="filled"
+                    id="url-input"
+                    placeholder="ex. images/photo.jpg"
+                    margin="dense"
+                    value={photoPath}
+                    color="secondary"
+                    onChange={e => setPhotoPath(e.target.value)}
+                />
+                <TextField
+                    label="Description:"
+                    variant="filled"
+                    id="description-input"
+                    margin="dense"
+                    color="secondary"
+                    value={photoDescription}
+                    onChange={e => setPhotoDescription(e.target.value)}
+                />
+                <Button
+                    id="submit-btn"
+                    type="submit"
+                    variant="contained"
+                    color="light"
+                    onClick={addPhoto}
+                >
+                    Submit
+            </Button>
+            </form>
+        </Drawer>
     )
 } // end GalleryForm
 

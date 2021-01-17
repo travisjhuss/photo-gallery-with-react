50a4c795be540a24cfcd2750b0333f3df3cb030a
Travis Huss <travisjhuss@gmail.com>
2021-01-17 08:55:41 -0600

    header style finished

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,16 @@
 import AppBar from '@material-ui/core/AppBar';
 import Typography from '@material-ui/core/Typography';
-import App from '../App/App';
+import PetsIcon from '@material-ui/icons/Pets';
+import './Header.css';
 
 function Header() {
     return (
-        <AppBar >
-            <Typography variant="h3">
-                My Best Friend, Morton.
+        <AppBar id="header">
+            <Typography variant="h3" id="title">
+                My Best Friend, Morton. 
             </Typography>
-            <Typography variant="subtitle1">
-                - A Life in Still Images
+            <Typography variant="subtitle1" id="subtitle">
+                - A Life in Stills <PetsIcon />
             </Typography>
         </AppBar>
     )
